388e25e4dd1e2aa7db5b228454f21423c962341a
Antonio Richard <114769420+antonioorichard@users.noreply.github.com>
2024-07-19 11:40:09 -0300

    Update handler.py
    Trocando o nome do modelo para o salvado.

--- handler.py
+++ handler.py
@@ -4,7 +4,7 @@ from flask import Flask, request, Response
 from healthinsurance.HealthInsurance import HealthInsurance
 
 # loading model
-model = pickle.load( open ('models/model_health_insurance.pkl', 'rb' ))
+model = pickle.load( open ('models/model_linear_regression.pkl', 'rb' ))
 
 # initialize API
 app = Flask ( __name__ )
