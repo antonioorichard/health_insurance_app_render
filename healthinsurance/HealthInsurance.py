80e4615a649ec01834f86ddcf9788aa82b62aea7
Antonio Richard <114769420+antonioorichard@users.noreply.github.com>
2024-07-21 15:37:24 -0300

    Update HealthInsurance.py

--- healthinsurance/HealthInsurance.py
+++ healthinsurance/HealthInsurance.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 import pandas as pd
-
+from sklearn.preprocessing import RobustScaler, MinMaxScaler
 
 
 
@@ -12,7 +12,7 @@ class HealthInsurance:
       self.age_scaler                              = pickle.load( open( self.home_path + 'parameter/Age_scaler.pkl', 'rb'))
       self.vintage_scaler                          = pickle.load( open( self.home_path + 'parameter/vintage_scaler.pkl', 'rb'))
       self.months_with_us_scaler                   = pickle.load( open( self.home_path + 'parameter/months_with_us_scaler.pkl', 'rb' ))
-      self.region_code_scaler                      = pickle.load( open( self.home_path + 'parameter/region_code_scaler.pkl', 'rb'))
+     # self.region_code_scaler                      = pickle.load( open( self.home_path + 'parameter/region_code_scaler.pkl', 'rb'))
       self.fe_policy_sales_channel_scaler          = pickle.load( open( self.home_path + 'parameter/policy_sales_channel_scaler.pkl', 'rb'))
       self.vehicle_age_1_2_Year_scaler             = pickle.load( open( self.home_path + 'parameter/vehicle_age_1_2_Year_scaler.pkl', 'rb'))
       self.vehicle_age_menor_q_1_Year_scaler       = pickle.load( open( self.home_path + 'parameter/vehicle_age_menor_q_1_Year_scaler.pkl', 'rb'))
@@ -47,7 +47,8 @@ class HealthInsurance:
 
   def feature_engineering(self, df2):
     # 2.0 Feature Engineering
-
+    rs = RobustScaler()
+    df2['region_code'] = rs.fit_transform( df2[['region_code']].values )
     # Vehicle Damage Number
 
   #  df2['vehicle_damage'] = df2['vehicle_damage'].apply( lambda x: 1 if x == 'Yes' else 0)
@@ -66,7 +67,7 @@ class HealthInsurance:
     # competition distance
     df5['annual_premium']        = self.annual_premium_scaler.transform( df5[['annual_premium']].values )
 
-    df5['region_code']           = self.region_code_scaler.transform( df5[['region_code']].values )
+    #df5['region_code']           = self.region_code_scaler.transform( df5[['region_code']].values )
 
     df5['policy_sales_channel']  = self.fe_policy_sales_channel_scaler.transform( df5[['policy_sales_channel']].values )
 
